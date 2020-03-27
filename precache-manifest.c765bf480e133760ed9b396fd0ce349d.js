cfddcd78a8ec3e61e2e0c215eed36adcd66ca861
amit-keshri <sonunikki8819@gmail.com>
2020-03-27 23:33:40 +0530

    build

--- precache-manifest.c765bf480e133760ed9b396fd0ce349d.js
+++ precache-manifest.c765bf480e133760ed9b396fd0ce349d.js
@@ -5,26 +5,26 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "a7a3bdfa3c2b2aaa672c",
-    "url": "/static/css/main.5f361e03.chunk.css"
+    "url": "/build/static/css/main.5f361e03.chunk.css"
   },
   {
     "revision": "f384dafb553245ab2c4c",
-    "url": "/static/js/2.ae5e556e.chunk.js"
+    "url": "/build/static/js/2.ae5e556e.chunk.js"
   },
   {
     "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
-    "url": "/static/js/2.ae5e556e.chunk.js.LICENSE.txt"
+    "url": "/build/static/js/2.ae5e556e.chunk.js.LICENSE.txt"
   },
   {
     "revision": "a7a3bdfa3c2b2aaa672c",
-    "url": "/static/js/main.e5f27d1c.chunk.js"
+    "url": "/build/static/js/main.e5f27d1c.chunk.js"
   },
   {
     "revision": "4bfda7dace0f55d808f2",
-    "url": "/static/js/runtime-main.fa962b67.js"
+    "url": "/build/static/js/runtime-main.fa962b67.js"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    "url": "/build/static/media/logo.5d5d9eef.svg"
   }
 ]);
