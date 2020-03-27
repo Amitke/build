8ee593eb482b441ae4c4cabad58ed08cfcc16a45
amit-keshri <sonunikki8819@gmail.com>
2020-03-27 23:05:25 +0530

    build

---
+++ build/precache-manifest.c765bf480e133760ed9b396fd0ce349d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ce5d897ed66239bc0055a91f1b86db1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7a3bdfa3c2b2aaa672c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f384dafb553245ab2c4c",
+    "url": "/static/js/2.ae5e556e.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/static/js/2.ae5e556e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a3bdfa3c2b2aaa672c",
+    "url": "/static/js/main.e5f27d1c.chunk.js"
+  },
+  {
+    "revision": "4bfda7dace0f55d808f2",
+    "url": "/static/js/runtime-main.fa962b67.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
